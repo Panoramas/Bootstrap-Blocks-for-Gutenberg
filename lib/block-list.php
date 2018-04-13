8634afaf5dc82ddeb851adf6e02b5488b5f94acb
Emmanuel Honoré <emmanuelhonore@gmail.com>
2018-04-12 23:42:55 -0500

    Add block-list.php

---
+++ lib/block-list.php
@@ -0,0 +1,19 @@
+<?php
+
+  /**
+   * $blocksList A list of all the blocks provided by bsgut
+   * @var array
+   */
+  var $blocksList = array(
+        "first block" => array(
+          "name" => __("First block", "bsgut"),
+          "dir" => "first-block",
+          "type" => "content",
+          "replace" => "core/Audio"
+        ),
+        "alert" => array(
+          "name" => __("Alert", "bsgut"),
+          "dir" => "alert-block",
+          "type" => "content"
+        )
+      );
