0d71238371e3c46593fe98ae75386b7e1010b60c
Laurent <laurent@resonne.fr>
2018-04-17 20:44:40 +0200

    Additional fix on template file

--- templates/block.js
+++ templates/block.js
@@ -1,6 +1,6 @@
 import { >>DEP2<< } from '@wordpress/components' // Generic, reusable UI WordPress components
 import { __ } from '@wordpress/i18n' // Internationalization utilities
-import { Fragment } from '@wordpress/element' // Abstraction on top of React
+import React, { Fragment } from '@wordpress/element' // Abstraction on top of React
 // import {} from '@wordpress/date' // Date formatting and manipulation utilities
 import { >>DEP1<< } from '@wordpress/blocks' // Module providing utilities for registering and building blocks
 // import {} from '@wordpress/data' // Abstraction on top of Redux
