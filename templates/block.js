aca1da50d0a62b8399ed2ce8a8d8be3e9ef2a38e
Laurent <laurent@resonne.fr>
2018-04-17 20:27:40 +0200

    Lint template file

--- templates/block.js
+++ templates/block.js
@@ -1,26 +1,29 @@
-const { __ } = wp.i18n; // Import __() from wp.i18n
-
-import { >>DEP1<< } from "@wordpress/blocks";
-import { Fragment } from "@wordpress/element";
-import { >>DEP2<< } from "@wordpress/components";
+import { >>DEP2<< } from '@wordpress/components' // Generic, reusable UI WordPress components
+import { __ } from '@wordpress/i18n' // Internationalization utilities
+import { Fragment } from '@wordpress/element' // Abstraction on top of React
+// import {} from '@wordpress/date' // Date formatting and manipulation utilities
+import { >>DEP1<< } from '@wordpress/blocks' // Module providing utilities for registering and building blocks
+// import {} from '@wordpress/data' // Abstraction on top of Redux
+// import {} from '@wordpress/editor' // Module representing the WordPress Editor’s page
+// import {} from '@wordpress/utils' // Various generic utilities
 >>CSS<<
 
-registerBlockType("bsgut/>>FOLDER<<", {
-  title: __(">>NAME<<"),
-  icon: ">>ICON<<",
-  category: "common",
-  description: __(">>DESCRIPTION<<"),
-  keywords: [ __("bootstrap"), __("bsgut"), __(">>KEYWORD<<") ],
+registerBlockType('bsgut/>>FOLDER<<', {
+  title: __('>>NAME<<'),
+  icon: '>>ICON<<',
+  category: 'common',
+  description: __('>>DESCRIPTION<<'),
+  keywords: [ __('bootstrap'), __('bsgut'), __('>>KEYWORD<<') ],
   attributes: {
     attr: {
-      type: "string"
+      type: 'string'
     }
   },
 
-  edit({ className, attributes, setAttributes, isSelected }) {
-    const { attr } = attributes;
+  edit ({ className, attributes, setAttributes, isSelected }) {
+    const { attr } = attributes
 
-    const updateAttr = attr => setAttributes({ attr });
+    const updateAttr = attr => setAttributes({ attr })
 
     return (
       <Fragment>
@@ -29,20 +32,20 @@ registerBlockType("bsgut/>>FOLDER<<", {
 
         {isSelected && (
           <InspectorControls key="inspector">
-            <PanelBody title={__( '">>NAME<< Settings' )}>
+            <PanelBody title={__('>>NAME<< Settings')}>
             </PanelBody>
           </InspectorControls>
         )}
       </Fragment>
-    );
+    )
   },
 
-  save({ attributes }) {
-    const { attr } = attributes;
+  save ({ attributes }) {
+    const { attr } = attributes
 
     return (
       <div className={}>
       </div>
-    );
+    )
   }
-});
+})
