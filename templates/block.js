388cb8c58b6e465db216e2e92a47293844739b46
Laurent <laurent@resonne.fr>
2018-05-08 12:16:11 +0200

    Update plop template

--- templates/block.js
+++ templates/block.js
@@ -1,51 +1,63 @@
-import { >>DEP2<< } from '@wordpress/components' // Generic, reusable UI WordPress components
-import { __ } from '@wordpress/i18n' // Internationalization utilities
-import React, { Fragment } from '@wordpress/element' // Abstraction on top of React
-// import {} from '@wordpress/date' // Date formatting and manipulation utilities
-import { >>DEP1<< } from '@wordpress/blocks' // Module providing utilities for registering and building blocks
-// import {} from '@wordpress/data' // Abstraction on top of Redux
-// import {} from '@wordpress/editor' // Module representing the WordPress Editor’s page
-// import {} from '@wordpress/utils' // Various generic utilities
+/**
+ * External dependencies
+ */
+// import { join } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { >>DEP2<< } from '@wordpress/components'; // Generic, reusable UI WordPress components
+import { __ } from '@wordpress/i18n'; // Internationalization utilities
+import { Fragment } from '@wordpress/element'; // Abstraction on top of React
+// import {} from '@wordpress/date'; // Date formatting and manipulation utilities
+import { >>DEP1<< } from '@wordpress/blocks'; // Module providing utilities for registering and building blocks
+// import {} from '@wordpress/data'; // Abstraction on top of Redux
+// import {} from '@wordpress/editor'; // Module representing the WordPress Editor’s page
+// import {} from '@wordpress/utils'; // Various generic utilities
+
+/**
+ * Internal dependencies
+ */
 >>CSS<<
 
-registerBlockType('bsgut/>>FOLDER<<', {
-  title: __('>>NAME<<'),
-  icon: '>>ICON<<',
-  category: 'common',
-  description: __('>>DESCRIPTION<<'),
-  keywords: [ __('bootstrap'), __('bsgut'), __('>>KEYWORD<<') ],
-  attributes: {
-    attr: {
-      type: 'string'
-    }
-  },
+registerBlockType( 'bsgut/>>FOLDER<<', {
+	title: __( '>>NAME<<' ),
+	icon: '>>ICON<<',
+	category: 'common',
+	description: __( '>>DESCRIPTION<<' ),
+	keywords: [ __( 'bootstrap' ), __( 'bsgut' ), __( '>>KEYWORD<<' ) ],
+	attributes: {
+		attr: {
+			type: 'string'
+		}
+	},
 
-  edit ({ className, attributes, setAttributes, isSelected }) {
-    const { attr } = attributes
+	edit({ className, attributes, setAttributes, isSelected }) {
+		const { attr } = attributes;
 
-    const updateAttr = attr => setAttributes({ attr })
+		const updateAttr = attr => setAttributes({ attr });
 
-    return (
-      <Fragment>
-        <div className={className}>
-        </div>
+		return (
+			<Fragment>
+				<div className={className}>
+				</div>
 
-        {isSelected && (
-          <InspectorControls key="inspector">
-            <PanelBody title={__('>>NAME<< Settings')}>
-            </PanelBody>
-          </InspectorControls>
-        )}
-      </Fragment>
-    )
-  },
+				{isSelected && (
+					<InspectorControls key="inspector">
+						<PanelBody title={__( '>>NAME<< Settings' )}>
+						</PanelBody>
+					</InspectorControls>
+				)}
+			</Fragment>
+		);
+	},
 
-  save ({ attributes }) {
-    const { attr } = attributes
+	save({ attributes }) {
+		const { attr } = attributes;
 
-    return (
-      <div className={}>
-      </div>
-    )
-  }
-})
+		return (
+			<div className={}>
+			</div>
+		);
+	}
+});
